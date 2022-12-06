1056bba9221f125b36d4663f5374b12aa86144e0
Daniel Sanchez <4942637+dsGCloud@users.noreply.github.com>
2022-12-05 18:04:46 -0600

    fleet network added

--- fleet.tf
+++ fleet.tf
@@ -18,12 +18,14 @@ module "fleet_vpc" {
   source  = "terraform-google-modules/network/google"
   version = "~> 5.1.0"
 
-  project_id                             = module.fleet_project.name
-  network_name                           = "${module.fleet_project.name}-fleet-vpc" 
+  project_id                             = module.fleet_network_project
+  network_name                           = "${module.fleet_project.name}-fleet-vpc"
+  routing_mode = "REGIONAL"
+
   delete_default_internet_gateway_routes = true
 
-  subnets = var.fleet_vpc_subnets
-  secondary_ranges = local.restricted_vpc_secondary_ranges
+  subnets = [var.fleet_subnet]
+  # secondary_ranges = local.restricted_vpc_secondary_ranges
 }
 
 module "fleet_service_accounts" {
@@ -45,6 +47,104 @@ module "fleet_service_accounts" {
     "${each.key}=>roles/stackdriver.resourceMetadata.writer",
     "${each.key}=>roles/opsconfigmonitoring.resourceMetadata.writer",
   ]
+}
+
+resource "google_compute_router" "fleet-router" {
+  name    = "edge-fleet-${var.fleet_subnet.subnet_region}"
+  region  = var.fleet_subnet.subnet_region
+  network = module.vpc.network_id
+  project = var.fleet_network_project
+
+  bgp {
+    asn = "64519"
+  }
+}
+
+variable "vpn_peers" {
+  default = {
+    "cluster0" = {
+      peer_ips      = ["8.8.8.8", "8.8.4.4"]
+      shared_secret = "foobarbazquux"
+      router_ips = {
+        interface1 = {
+          ip_range = "169.254.0.1/30"
+          peer_ip  = "169.254.0.2"
+        }
+        interface2 = {
+          ip_range = "169.254.1.1/30"
+          peer_ip  = "169.254.1.2"
+        }
+      }
+    }
+    "cluster1" = {
+      peer_ips      = ["1.1.1.1", "1.0.0.1"]
+      shared_secret = "foobarbazquux"
+      router_ips = {
+        interface1 = {
+          ip_range = "169.254.2.1/30"
+          peer_ip  = "169.254.2.2"
+        }
+        interface2 = {
+          ip_range = "169.254.3.1/30"
+          peer_ip  = "169.254.3.2"
+        }
+      }
+    }
+  }
+}
+
+resource "google_compute_ha_vpn_gateway" "ha_gateway" {
+  project = var.fleet_network_project
+  region  = "us-central1"
+  name    = "fleet-gw-us-central1"
+  network = module.vpc.network_id
+}
+
+resource "google_compute_external_vpn_gateway" "peer" {
+  for_each = var.vpn_peers
+
+  project         = var.fleet_network_project
+  name            = each.key
+  redundancy_type = "TWO_IPS_REDUNDANCY"
+  description     = "An externally managed VPN gateway"
+  dynamic "interface" {
+    for_each = each.value.peer_ips
+    content {
+      id         = interface.key
+      ip_address = interface.value
+    }
+  }
+}
+
+output "peers" {
+  value = google_compute_external_vpn_gateway.peer
+}
+
+module "tunnels" {
+  source = "./tunnels"
+
+  for_each = var.vpn_peers
+
+  cluster_name  = each.key
+  project_id    = var.fleet_network_project
+  region        = "us-central1"
+  router        = google_compute_router.fleet-router.id
+  vpn_gw        = google_compute_ha_vpn_gateway.ha_gateway.id
+  peer_gw       = google_compute_external_vpn_gateway.peer[each.key].id
+  shared_secret = each.value.shared_secret
+}
+
+module "bgp" {
+  source = "./bgp"
+
+  for_each = var.vpn_peers
+
+  cluster_name = each.key
+  project_id   = var.fleet_network_project
+  region       = "us-central1"
+  router       = google_compute_router.fleet-router.name
+  tunnels      = module.tunnels[each.key]
+  router_ips   = each.value.router_ips
 }
 
 module "vm_instance_template" {
@@ -72,7 +172,7 @@ module "vm_instance_template" {
   tags                 = var.tags
 }
 
-module "vm_compute_instance_factory" {
+module "cluster_factory" {
   source  = "terraform-google-modules/vm/google//modules/compute_instance"
   version = "7.8.0"
 
@@ -82,54 +182,4 @@ module "vm_compute_instance_factory" {
   num_instances      = var.REQUIRED_CLUSTER_SIZE
   subnetwork_project = data.terraform_remote_state.project_factory.outputs.host_projects.project_id
   hostname           = var.hostname_prefix
-}
-
-module "vpn_ha" {
-  source                = "terraform-google-modules/vpn/google//modules/vpn_ha"
-  version               = "2.2.0"
-
-  project_id            = module.base_network_vpc.project_id
-  region                = var.vpn_region
-  network               = module.base_network_vpc.network_name
-  name                  = var.vpn_name
-  peer_external_gateway = {
-    redundancy_type = "TWO_IPS_REDUNDANCY"
-    interfaces = [
-      {
-        id         = 0
-        ip_address = var.secondary_onprem_ip # on-prem router ip address
-      },
-      {
-        id         = 1
-        ip_address = var.primary_onprem_ip # on-prem router ip address
-      }
-    ]
-  }
-  router_asn = 64514
-  tunnels = {
-    remote-0 = {
-      bgp_peer = {
-        address = "169.254.1.1"
-        asn     = 64513
-      }
-      bgp_peer_options                = null
-      bgp_session_range               = "169.254.1.2/30"
-      ike_version                     = 2
-      vpn_gateway_interface           = 0
-      peer_external_gateway_interface = 0
-      shared_secret                   = var.shared_secret_1
-    }
-    remote-1 = {
-      bgp_peer = {
-        address = "169.254.2.1"
-        asn     = 64513
-      }
-      bgp_peer_options                = null
-      bgp_session_range               = "169.254.2.2/30"
-      ike_version                     = 2
-      vpn_gateway_interface           = 1
-      peer_external_gateway_interface = 0
-      shared_secret                   = var.shared_secret_2
-    }
-  }
 }
--- variables.tf
+++ variables.tf
@@ -3,8 +3,7 @@ variable "org_id" {
   type        = string
 }
 
-// A name for the new fleet that is to be deployed
-variable "fleet_folder_name" {
+variable "billing_account_id" {
   type = string
 }
 
@@ -13,6 +12,52 @@ variable "region_folder_name" {
   type = string
 }
 
-variable "billing_account_id" {
+// A name for the new fleet that is to be deployed
+variable "fleet_name" {
   type = string
+}
+
+variable "fleet_network_project" {
+  type = string
+}
+
+variable "fleet_default_region" {
+  default = "us-central1"
+  type    = string
+}
+
+variable "network_name" {
+  default = "fleetnet"
+  type    = string
+}
+
+variable "fleet_router_default_asn" {
+  default = "64519"
+  type    = string
+}
+
+variable "fleet_subnet" {
+  description = "subnet to create"
+  type = object({
+    subnet_name   = string
+    subnet_ip     = string
+    subnet_region = string
+  })
+  default = {
+    subnet_name   = "fleet-us-central1",
+    subnet_ip     = "172.16.100.0/24",
+    subnet_region = "us-central1"
+  }
+}
+
+variable "fleet_secondary_ranges" {
+  type = list
+}
+
+variable "labels" {
+  type = map
+}
+
+variable "metadata" {
+  type = map
 }
