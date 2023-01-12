a797bde935f42524428345d4b867351646225139
Paul Durivage <durivage@google.com>
2023-01-12 14:43:44 -0700

    remove SDS project and consolidate org/project creation

--- fleet/outputs.tf
+++ fleet/outputs.tf
@@ -13,10 +13,6 @@ output "control_plane_service_account_project" {
   value = module.control_plane_service_account_project
 }
 
-output "control_plane_sds_project" {
-  value = module.control_plane_sds_project
-}
-
 output "fleet_project" {
   value = module.fleet_project
 }
